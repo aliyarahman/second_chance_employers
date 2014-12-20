b87d99613a53c4384c6911c12aa1e366266436a4
Rachel Shorey <rshorey@gmail.com>
2014-12-20 12:56:22 -0500

    adding gh-pages index

--- second_chance/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from app.models import *
-
-# Register your models here.
-admin.site.register(Employer)
-admin.site.register(Position)
-admin.site.register(Industry)
-admin.site.register(UserReview)
